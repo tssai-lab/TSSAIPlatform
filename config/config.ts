9a45c9899d6bc9bfd140f1ae49eac39136c9ec99
Zewen <czw20040417@163.com>
2026-04-25 11:01:20 +0800

    feat: 更改项目标题

--- config/config.ts
+++ config/config.ts
@@ -83,7 +83,7 @@ export default defineConfig({
    * @name layout 插件
    * @doc https://umijs.org/docs/max/layout-menu
    */
-  title: 'Ant Design Pro',
+  title: 'AI训练管理',
   layout: {
     locale: true,
     ...defaultSettings,
--- src/pages/user/login/index.tsx
+++ src/pages/user/login/index.tsx
@@ -23,7 +23,6 @@ import { Alert, App, Tabs } from 'antd';
 import { createStyles } from 'antd-style';
 import React, { useEffect, useState } from 'react';
 import { flushSync } from 'react-dom';
-import { Footer } from '@/components';
 import { login } from '@/services/ant-design-pro/api';
 import { getFakeCaptcha } from '@/services/ant-design-pro/login';
 import { STORAGE_KEYS, storage } from '@/utils/storage';
@@ -413,7 +412,6 @@ const Login: React.FC = () => {
           </div>
         </LoginForm>
       </div>
-      <Footer />
     </div>
   );
 };
