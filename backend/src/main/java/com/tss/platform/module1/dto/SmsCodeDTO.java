1b02acb0831ffca0a89916967353a3b8cf9df3c0
qqh123456 <115910964+qqh123456@users.noreply.github.com>
2026-05-12 01:24:29 +0800

    Update SmsCodeDTO.java

--- backend/src/main/java/com/tss/platform/module1/dto/SmsCodeDTO.java
+++ backend/src/main/java/com/tss/platform/module1/dto/SmsCodeDTO.java
@@ -6,7 +6,7 @@ import lombok.Data;
 
 @Data
 public class SmsCodeDTO {
-    @NotBlank(message = "Mobile must not be blank")
-    @Pattern(regexp = "^1[3-9]\\d{9}$", message = "Invalid mobile format")
+    @NotBlank(message = "手机号不能为空")
+    @Pattern(regexp = "^1[3-9]\\d{9}$", message = "手机号格式错误")
     private String mobile;
 }
