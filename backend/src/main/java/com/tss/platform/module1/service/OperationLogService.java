03c6f758ed21cd575d3c40523b76b5942ff54bfb
qqh123456 <115910964+qqh123456@users.noreply.github.com>
2026-05-16 12:14:04 +0800

    Update OperationLogService.java

--- backend/src/main/java/com/tss/platform/module1/service/OperationLogService.java
+++ backend/src/main/java/com/tss/platform/module1/service/OperationLogService.java
@@ -4,6 +4,6 @@ import com.baomidou.mybatisplus.extension.service.IService;
 import com.tss.platform.module1.entity.OperationLog;
 
 public interface OperationLogService extends IService<OperationLog> {
-    // 记录操作日志
     boolean recordLog(OperationLog log);
+    IPage<OperationLog> queryLogs(OperationLogQueryDTO queryDTO);
 }
