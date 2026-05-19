33c14bc0ba39c40f1a4a8f4ddc5bddeb555b1e5b
qqh123456 <115910964+qqh123456@users.noreply.github.com>
2026-05-19 22:16:23 +0800

    Create OperationType.java

---
+++ backend/src/main/java/com/tss/platform/module1/common/OperationType.java
@@ -0,0 +1,10 @@
+package com.example.demo.common;
+
+public class OperationType {
+    public static final String ADD = "1";
+    public static final String DELETE = "2";
+    public static final String UPDATE = "3";
+    public static final String RESET = "4";
+    public static final String LOGIN = "5";
+    public static final String LOGOUT = "6";
+}
