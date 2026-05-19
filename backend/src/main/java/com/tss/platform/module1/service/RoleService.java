f6c4f1d6fb54c7cbc58207ed417746af66f907b9
qqh123456 <115910964+qqh123456@users.noreply.github.com>
2026-05-19 22:44:14 +0800

    Create RoleService.java

---
+++ backend/src/main/java/com/tss/platform/module1/service/RoleService.java
@@ -0,0 +1,7 @@
+package com.tss.platform.module1.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.tss.platform.module1.entity.Role;
+
+public interface RoleService extends IService<Role> {
+}
